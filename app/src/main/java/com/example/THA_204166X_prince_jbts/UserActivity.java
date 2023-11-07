82ea4224a9549ab38078e1afbbec55552d3daac8
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-11-07 14:41:55 +0530

    added 55 files

---
+++ app/src/main/java/com/example/THA_204166X_prince_jbts/UserActivity.java
@@ -0,0 +1,38 @@
+package com.example.THA_204166X_prince_jbts;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.THA_204166X_prince_jbts.databinding.ActivityUserBinding;
+
+public class UserActivity extends AppCompatActivity {
+
+    ActivityUserBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+
+        if (intent != null){
+
+            String name = intent.getStringExtra("name");
+            String phone = intent.getStringExtra("phone");
+            String country = intent.getStringExtra("country");
+            int imageid = intent.getIntExtra("imageid",R.drawable.a);
+
+            binding.nameProfile.setText(name);
+            binding.phoneProfile.setText(phone);
+            binding.countryProfile.setText(country);
+            binding.profileImage.setImageResource(imageid);
+
+
+        }
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "THA_APP_204166X_Prince_JBTS"
+include(":app")
+ 
